422802a1ff1b5d2a257cdd668bff2f4d72d207cd
Jensone <jeancharlesjensone@gmail.com>
2022-05-11 22:14:10 +0200

    add: Complete sidebar html, css and js code

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,10 @@
 let menuBurger = document.querySelector(".menu_burger")
 let sideMenu = document.querySelector(".side_menu")
+let openMenu = document.querySelector(".open_menu")
+let closeMenu = document.querySelector(".close_menu")
 
 menuBurger.addEventListener("click", e => {
     sideMenu.classList.toggle("active")
+    openMenu.classList.toggle("active")
+    closeMenu.classList.toggle("active")
 })
