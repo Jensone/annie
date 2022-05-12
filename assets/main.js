63e6972845e20dddf7f13d8aef9cabf217e63e04
Jensone <jeancharlesjensone@gmail.com>
2022-05-12 13:22:22 +0200

    Add: JS code for explanation

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,28 @@
 let menuBurger = document.querySelector(".menu_burger")
 let sideMenu = document.querySelector(".side_menu")
-let openMenu = document.querySelector(".open_menu")
-let closeMenu = document.querySelector(".close_menu")
+// let openMenu = document.querySelector(".open_menu")
+// let closeMenu = document.querySelector(".close_menu")
 
 menuBurger.addEventListener("click", e => {
     sideMenu.classList.toggle("active")
-    openMenu.classList.toggle("active")
-    closeMenu.classList.toggle("active")
+    // openMenu.classList.toggle("active")
+    // closeMenu.classList.toggle("active")
+})
+
+
+
+
+
+
+
+
+// Je stock l'icone pour lancer une surveillance au survol
+let login = document.querySelector(".login")
+
+// Je stock logo pour le faire disparaitre au survol de login
+let logo = document.querySelector(".logo")
+
+login.addEventListener("mouseover", e => {
+    console.log("Youpi ça marche")
+    logo.style.backgroundColor = 'gray'
 })
